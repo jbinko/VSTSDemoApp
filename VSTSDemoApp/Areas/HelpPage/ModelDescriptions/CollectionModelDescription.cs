dde996ca7d174bfd477e0a1187d16cef2678a2fb
Jiri Binko <jiri_binko@msn.com>
2017-09-24 23:18:11 +0200

    Add project files.

---
+++ VSTSDemoApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace VSTSDemoApp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ VSTSDemoApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VSTSDemoApp.MvcApplication" Language="C#" %>
